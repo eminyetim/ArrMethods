6e4634c36a8b614a7b9380bba0bb284b0a98d514
Emin YTM <eminyetim25@gmail.com>
2023-03-13 02:40:51 +0300

    array

--- Program.cs
+++ Program.cs
@@ -1,48 +1,91 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-class program
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+        int again = 0;
+        int againBefore = 0;
+        foreach (var item in arr)
+        {   
+            
+            if(againBefore < again)
+            {
+                againBefore = again;
+            }
+            again = 0;
+            foreach (var item2 in arr)
+            {
+                
+                if(item == item2)
+                {
+                    again++;
+                }    
+            }         
+        }
+        return againBefore;
+    }
+    
+
+}
+
+class Solution
 {
     public static void Main(string[] args)
     {
-        int[] numberArray = {5 , 4 , 3 , 8 , 12 , 25 ,61};
+        // //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        // int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        // List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        // int result = Result.migratoryBirds(arr);
+        // System.Console.WriteLine(result);
+        // textWriter.WriteLine(result);
+
+        // textWriter.Flush();
+        // textWriter.Close();
+
+
+        string[] numbers = new string[10];
+        numbers[0] = "0";
+        numbers[1] = "1";
+        numbers[2] = "2";
+        numbers[3] = "3";
+        numbers[2] = null;
+
+
         
-        System.Console.WriteLine("** Unordered List **");
-        foreach (var item in numberArray)
-        {
-            System.Console.WriteLine(item);
-        }
+        
 
-        System.Console.WriteLine("**Ordered List**");
-        Array.Sort(numberArray);
-        foreach (var item in numberArray)
-        {
-            System.Console.WriteLine(item);
-        }
-
-        System.Console.WriteLine("**Clear**");
-        Array.Clear(numberArray,2,2);
-        foreach (var item in numberArray)
-        {
-            System.Console.WriteLine(item);
-        }
-
-        System.Console.WriteLine("**Reverse**");
-        Array.Reverse(numberArray);
-        foreach (var item in numberArray)
-        {
-            System.Console.WriteLine(item);
-        }
-   
-        System.Console.WriteLine("**Index Of**");
-        System.Console.WriteLine(Array.IndexOf(numberArray,3));
-
-        System.Console.WriteLine("**Resize**");
-        Array.Resize<int>(ref numberArray , 15);
-        numberArray[13] =88;
-        foreach (var item in numberArray)
-        { 
-            System.Console.WriteLine(item);
-        }
+       for(int i = 0 ;  i < numbers.Count() ; i++)
+       {
+            if(numbers[i] != null)
+            {
+                System.Console.WriteLine(numbers[i]);
+            }
+            
+       }
+                
     }
-    
 }
